eef7bfd341400d997e5c53230e0f3b8cfcd87cd0
zhangzhen <1499455271@qq.com>
2020-01-03 12:00:00 +0800

    完成登录系统和个人信息页面

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from app import app
 from flask import render_template, redirect, url_for, flash
 from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
-from app.models import User
+from flask_login import current_user, login_user, logout_user,login_required
+from app.models import User,load_user
 
 
 @app.route('/')
@@ -11,7 +11,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -28,3 +28,18 @@ def login():
     #   return render_template('userInfo.html',user={'name':form.username.data})
     else:
         return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/userInfo')
+@login_required
+def userInfo():
+    #用户未登录，重定向到登录界面
+    if not current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    return render_template('userInfo.html' )
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return  render_template('index.html')
--- app/test/testDB.py
+++ app/test/testDB.py
@@ -2,7 +2,7 @@ from app import app
 from app import db
 from app.models import User
 
-u = User(username='aaa',email='',address='',phone='')
+u = User(username='root',email='123@abc.com',address='aaa',phone='aaa')
 u.set_password('aaa')
 db.session.add(u)
 db.session.commit()
