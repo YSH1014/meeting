8dea4d3f5d054e0f7aa03cf910e84f95cae2b559
zhangzhen <1499455271@qq.com>
2020-01-07 19:21:26 +0800

    取消username的唯一性

--- app/models.py
+++ app/models.py
@@ -19,7 +19,7 @@ class MeetingStatusType(Enum):
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    username = db.Column(db.String(64), index=True,  nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
     address = db.Column(db.String(150), nullable=False)
