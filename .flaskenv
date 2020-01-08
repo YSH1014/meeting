a42785c92b969b3e5ea1d86c4077994e7e4d76bf
zhangzhen <1499455271@qq.com>
2020-01-08 14:43:06 +0800

    针对生产环境使用postgre数据库

--- .flaskenv
+++
@@ -1,2 +0,0 @@
-FLASK_APP=meeting.py
-FLASK_ENV=development
--- app/config.py
+++ app/config.py
@@ -1,8 +1,19 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
+    POSTGRES_URL = os.environ.get("POSTGRES_URL")
+    POSTGRES_USER = os.environ.get("POSTGRES_USER")
+    POSTGRES_PW = os.environ.get("POSTGRES_PW")
+    POSTGRES_DB = os.environ.get("POSTGRES_DB")
+
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = ( \
+        (os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'app.db')))  # 开发模式使用sqlite
+        if os.environ.get('FLASK_ENV') == 'development' \
+            else 'postgresql+psycopg2://{user}:{pw}@{url}/{db}' \
+            .format(user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL, db=POSTGRES_DB) #否则使用postgre
+        )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
