a5ad3473935354a0982d74864159fe0f22e12949
xx70235 <erkofs@gmail.com>
2020-01-29 22:19:15 +0800

    update login to astro passport

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -7,62 +7,118 @@ from app.models import User, load_user, RoleType, MeetingStatusType, Meeting
 from app.security import user_required
 import sqlalchemy.exc
 import base64
+import re
+import pycurl
+import io
+import json
 
 
-@app.route('/login_new',methods = ['GET', 'POST'])
-def login_new():
+@app.route('/login',methods = ['GET', 'POST'])
+def login():
     if not request.cookies.get('china-vo'):
-        # res = make_response('cookie')
-        # res.set_cookie('cvoumt', '"https://nadc.china-vo.org/meetings/userInfo"', max_age=3600 * 24, path = '/', domain='china-vo.org')
         redirect_to_login  = redirect("http://passport.china-vo.org/loginFrm?umt=true")
         response = make_response(redirect_to_login)
-        # session["cvoumt"] = "\"https://nadc.china-vo.org/meetings/userInfo\""
-        response.set_cookie('cvoumt', "\"https://nadc.china-vo.org/meetings/login_new\"", max_age=3600 * 24, path = '/', domain='china-vo.org')
-        # response.set_cookie('cvoumt1', "\"test\"", max_age=3600 * 24, path = '/', domain='china-vo.org')
+        # response.set_cookie('cvoumt', "\"https://nadc.china-vo.org/meetings/login\"", max_age=3600 * 24, path = '/', domain='china-vo.org')
+        response.set_cookie('cvoumt', "", max_age=3600 * 24, path = '/', domain='china-vo.org')
         return redirect_to_login
 
     else:
         token = request.cookies.get('china-vo')
-        print(token)
-        
-        decodeToken = retriveStrByBase64(token)
-        # TODO: session['token'] = decodeToken
-        email, token = decodeToken.split(':')
-
-
-def retriveStrByBase64(token):
-    decodeToken = base64.b64decode(token)
-    return decodeToken
-
-def getUserInfoByEmail(email):
-    #TODO: get userinfo from astrocloud
-    pass
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        # 返回错误信息
+        decodeToken = str(decode_base64(token))[2:-1]
+        cstnetId, phone, token = decodeToken.split(':')
+        user = User.query.filter_by(email=cstnetId).first()
         if user is None:
-            flash("用户不存在")
-            return redirect(url_for('login'))
-        elif not user.check_password(form.password.data):
-            flash('密码错误')
-            return redirect(url_for('login'))
+            try:
+                profile = getUserInfoByCstnetId(cstnetId)
+                user = createUser(profile)
+                login_user(user)
+                flash("登录成功")
+                return redirect(url_for('userInfo'))
+            except:
+                flash("登录失败")
+                return redirect(url_for('index'))
+        else:
+            login_user(user)
+            flash("登录成功")
+            return redirect(url_for('userInfo'))
 
-        #登录成功
-        login_user(user, remember=form.remember_me.data)
-        flash("登录成功")
-        return redirect(url_for('userInfo'))
+def decode_base64(data, altchars= '+/'):
+    data = re.sub(r'[^a-zA-Z0-9%s]+' % altchars, '', data)  # normalize
+    missing_padding = len(data) % 4
+    if missing_padding:
+        data += '='* (4 - missing_padding)
+    return base64.b64decode(data, altchars)
 
-    #   return render_template('userInfo.html',user={'name':form.username.data})
-    else:
-        return render_template('login.html', title='Sign In', form=form)
+def getUserInfoByCstnetId(cstnetId):
+    e = io.BytesIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, 'http://astrocloud.china-vo.org/services/userinfo?callback=searcht&id='+cstnetId)
+    c.setopt(c.WRITEFUNCTION, e.write)
+    c.setopt(c.HTTPHEADER, ['Content-Type: application/json','Accept-Charset: UTF-8'])
+    c.perform()
+    c.close()
+    profile = e.getvalue().decode('UTF-8')
+    print(profile)
+    return profile
+
+
+def createUser(profile):
+    ob = json.loads(profile[8:-1])
+    username = ob['truename']
+    print(username)
+    email = ob['cstnetId']
+    print(email)
+    password = ob['securityToken']
+    print(password)
+    address = 'no address info'
+    print(address)
+    phone = ob['phone']
+    print(phone)
+
+    user = User(
+            username=username,
+            email=email,
+            phone=phone,
+            address=address,
+            role=RoleType.USER
+        )
+    user.set_password(password)
+    print(user)
+    try:
+        db.session.add(user)
+        db.session.commit()
+        return user
+        # flash('Congratulations, you are now a registered user!')
+        # return redirect(url_for('login'))  # 注册成功，返回登录界面
+    except sqlalchemy.exc.IntegrityError as e:
+        print(e)
+    #     flash('新用户注册失败，请检查Email或手机是否已被注册')
+
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(email=form.email.data).first()
+#         # 返回错误信息
+#         if user is None:
+#             flash("用户不存在")
+#             return redirect(url_for('login'))
+#         elif not user.check_password(form.password.data):
+#             flash('密码错误')
+#             return redirect(url_for('login'))
+
+#         #登录成功
+#         login_user(user, remember=form.remember_me.data)
+#         flash("登录成功")
+#         return redirect(url_for('userInfo'))
+
+#     #   return render_template('userInfo.html',user={'name':form.username.data})
+#     else:
+#         return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/userInfo')
@@ -80,7 +136,17 @@ def userInfo():
 @user_required
 def logout():
     logout_user()
-    return render_template('index.html')
+    redirect_to_index  = redirect(url_for('index'))
+    response = make_response(redirect_to_index)
+    response.set_cookie('china-vo','', expires=0, path='/',domain="china-vo.org")
+    return redirect_to_index
+
+# @app.route('/logout_new')
+# @user_required
+# def logout_new():
+#     logout_user()
+
+#     return render_template('index.html')
 
 
 @app.route('/register', methods=['Get', 'Post'])
