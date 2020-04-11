e088ba5f65a30a8f8b1f0e6c6b629b0aca5ecc95
server <server@xxx.xxx>
2020-04-11 14:42:03 +0800

    在生产环境对cal.py进行修改

--- app/cal.py
+++ app/cal.py
@@ -16,6 +16,16 @@ def add_meeting_to_calendar(meeting):
     if meeting.start_date is None or meeting.end_date is None:
         return 
         
+    id = meeting.id,
+    title_CN=meeting.title if meeting.title is not None else "",
+    title_EN = meeting.title_EN if meeting.title_EN is not None else "",
+    theme = meeting.get_theme('zh_Hans_CN')
+    if theme is not None and theme !="":
+        theme = theme.replace('\r\n',r'\n')
+    start_date = meeting.start_date.strftime("%Y%m%d")
+    end_date = meeting.end_date.strftime("%Y%m%d")
+    country = meeting.get_country('zh_Hans_CN')
+    city = meeting.get_city('zh_Hans_CN')
     vcal = """
 BEGIN:VCALENDAR
 PRODID:-//Example Corp.//CalDAV Client//EN
@@ -30,14 +40,14 @@ LOCATION:{country}-{city}
 END:VEVENT
 END:VCALENDAR
 """.format(
-        id = meeting.id,
-        title_CN=meeting.title if meeting.title is not None or "",
-        title_EN = meeting.title_EN if meeting.title_EN is not None or "",
-        theme = meeting.get_theme('zh_Hans_CN').replace('\n',r"\n"),              #需要将转义的\n替换为纯文本的\n
-        start_date = meeting.start_date.strftime("%Y%m%d"),
-        end_date = meeting.end_date.strftime("%Y%m%d"),
-        country = meeting.get_country('zh_Hans_CN'),
-        city = meeting.get_city('zh_Hans_CN')
+        id = id,
+        title_CN=title_CN,
+        title_EN = title_EN,
+        theme = theme,
+        start_date = start_date,
+        end_date = end_date,
+        country = country,
+        city = city,
     )
 
     try:
